145e33f253f1602f1aff25f0e54a2b8dc16bf10d
GregS30 <gjschmitt@live.com>
2018-06-05 18:09:50 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+
+  ALL = []
+
+  attr_accessor :name, :weight, :height
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  def self.clear
+    ALL.clear
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+
+  ALL = []
+
+  attr_accessor :name, :type, :booty
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+
+    ALL << self
+
+  end
+
+  def self.all
+    ALL
+  end
+
+  def self.clear
+    ALL.clear
+  end
+
 end
