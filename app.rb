9dc933b6f2e3b60c88d6c07a9de2b840b9356052
github username <github email address>
2018-06-05 16:29:33 -0400

    solved not passing tests

--- app.rb
+++ app.rb
@@ -3,7 +3,20 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    #code other routes/actions here
+    get '/' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ships = Ship.all
+      params[:pirate][:ships].each do |individual_ship|
+          Ship.new(individual_ship)
+      end
+
+      erb :"pirates/show"
+    end
 
   end
 end
